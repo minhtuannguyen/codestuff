ee9c60c9a21d7a2fb08bce982c08d92518142645
Minh Tuan Nguyen <minhtuannguyen2704@gmail.com>
2015-12-26 22:32:07 +0100

    route network exc

--- src/main/java/de/hh/ex/RouteNetwork.java
+++ src/main/java/de/hh/ex/RouteNetwork.java
@@ -1,4 +1,37 @@
 package de.hh.ex;
 
+/**
+ * the input array represents a route network.
+ * uneven number means up
+ * uneven number means to the right
+ * Find one solution which scales with O(n) to find
+ * the first point of intersection of the route network
+ */
+
 public class RouteNetwork {
+
+    /* the first point of intersection is located in the array
+    *  where three positive number occur at the same time
+    */
+    static int find(int[] route) {
+        // translate to binary array: {-1, 2, 3, 4, 5, -6} -> {0, 1, 1, 1, 1, 0}
+        int[] binaryRoute = new int[route.length];
+        for (int i = 0; i < route.length; i++) {
+            binaryRoute[i] = route[i] > 0 ? 1 : 0;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < binaryRoute.length; i++) {
+            if (binaryRoute[i] == 0)
+                sum = 0;
+            else
+                sum += binaryRoute[i];
+
+            if (sum >= 3) {
+                return i;
+            }
+        }
+
+        throw new RuntimeException("failed");
+    }
 }
--- src/test/java/de/hh/ex/RouteNetworkTest.java
+++ src/test/java/de/hh/ex/RouteNetworkTest.java
@@ -1,7 +1,48 @@
 package de.hh.ex;
 
-import static org.testng.Assert.*;
+import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
+/**
+ * the input array represents a route network.
+ * uneven number means up
+ * uneven number means to the right
+ * Find one solution which scales with O(n) to find
+ * the first point of intersection of the route network
+ */
 public class RouteNetworkTest {
 
+    @Test
+    public void interceptPointIsThe4th() throws Exception {
+        //Given
+        int[] route = new int[]{-1, 2, 3, 4, 5, -6};
+
+        //When
+        int result = RouteNetwork.find(route);
+
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void interceptPointIsThe10() throws Exception {
+        //Given
+        int[] route = new int[]{-1, 2, -3, 4, -5, 6, -7, 8, 9, 10};
+
+        //When
+        int result = RouteNetwork.find(route);
+
+        assertEquals(9, result);
+    }
+
+    @Test
+    public void interceptPointIsThe11() throws Exception {
+        //Given
+        int[] route = new int[]{-1, 2, -3, -4, -5, 6, -7, -8, 9, 10, 11};
+
+        //When
+        int result = RouteNetwork.find(route);
+
+        assertEquals(10, result);
+    }
 }
